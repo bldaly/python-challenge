7a431a3c351f36376ea37b4843223dd6fb5841f9
Brenda Daly <Brenda2876@gmail.com>
2018-12-12 15:44:44 -0700

    Add files via upload

--- PyBank/mainpybank.py
+++ PyBank/mainpybank.py
@@ -5,29 +5,38 @@ import csv
 
 calendar = []
 dollar_value = []
-#average_change = []
+average_change = []
 csvpath = os.path.join('', 'Resources', 'budget_data.csv')
 
 
 with open(csvpath, newline='') as csvfile:
 
-   # CSV reader specifies delimiter and variable that holds contents
-   csvreader = csv.reader(csvfile, delimiter=',')
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
 
-   # print(csvreader)
+    # print(csvreader)
 
-   # Read the header row first (skip this step if there is now header)
-   csv_header = next(csvreader)
-   # print(f"CSV Header: {csv_header}")
-
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    # print(f"CSV Header: {csv_header}")
+    previous_row = next(csvreader)
+    calendar.append(previous_row[0])
+    value = int(previous_row[1])
+    max_dollar_value = int(previous_row[1])
+    changed_value = int(previous_row[1]) - int(previous_row[1])+1
+    dollar_value.append(value) 
    # Read each row of data after the header
-   for row in csvreader:
+    for row in csvreader:
        calendar.append(row[0])
        value = int(row[1])
        max_dollar_value = int(row[1])
        changed_value = int(row[1]) - int(row[1])+1
        dollar_value.append(value)
-    
+       change_value=int(row[1])-int(previous_row[1])
+       average_change.append(change_value)
+       previous_row = row 
+       
+change = sum(average_change)/len(average_change)
 month = len(calendar)
 total = sum(dollar_value)
 #max_value = max(dollar_value)
@@ -44,15 +53,7 @@ print("Financial Analysis")
 print("----------------------------")
 print("Total Months: " + str(month))
 print("Total: $" + str(total))
-print("Average Change: ")
-print("Greatest Increase in Profits: " + str(max(dollar_value)))
-print("Greatest Decrease in Profits: " + str(min(dollar_value)))
-
-
-# Print results
-#print("The total numbers of months included in the dataset is " + str(total_months) + " months!")
-#The total net amount of "Profit/Losses" over the entire period
-#The average change in "Profit/Losses" between months over the entire period
-#The greatest increase in profits (date and amount) over the entire period
-#The greatest decrease in losses (date and amount) over the entire period
+print("Average Change: " + str(change))
+print("Greatest Increase in Profits: " + str(max(average_change)))
+print("Greatest Decrease in Profits: " + str(min(average_change)))
 
